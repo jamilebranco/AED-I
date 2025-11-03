f52846af1cefc21a6b2609c987049a6f9a16bbc1
Jamile <jamilefbranco@gmail.com>
2025-11-03 00:35:16 -0300

    .

--- Trabalho Prático 1/CanPlaceFlowers_EmCasa.c
+++ Trabalho Prático 1/CanPlaceFlowers_EmCasa.c
@@ -5,22 +5,21 @@ Verifica se é possível plantar n flores no flowerbed sem quebrar a regra de ad
 ====================
 */
 
-bool CanPlaceFlowers( int *flowerbed, int flowerbedSize, int n )
-{
-	// percorre cada espaço do flowerbed enquanto ainda houver flores para plantar
-	for ( int i = 0; i < flowerbedSize && n > 0; i++ )
-	{
-		// verifica se é possível plantar na posição i
-		if ( ( ( i == 0 && ( flowerbedSize == 1 || flowerbed[1] == 0 ) ) ||
-			   ( i == flowerbedSize - 1 && flowerbed[flowerbedSize - 2] == 0 ) ||
-			   ( i > 0 && i < flowerbedSize - 1 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0 ) )
-			 && flowerbed[i] == 0 )
-		{
-			flowerbed[i] = 1;   // planta a flor
-			n--;				// decrementa apenas se plantar
-		}
-	}
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
 
-	// retorna true se todas as flores foram plantadas, false caso contrário
-	return n == 0;
+	// percorre cada espaço do flowerbed enquanto ainda houver flores para plantar
+    for (int i = 0; i < flowerbedSize && n > 0; i++) {
+
+		// verifica se é possível plantar na posição i
+        if (((i == 0 && (flowerbedSize == 1 || flowerbed[1] == 0)) ||
+            (i == flowerbedSize-1 && flowerbed[flowerbedSize-2] == 0) ||
+            (i > 0 && i < flowerbedSize-1 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0))
+            && flowerbed[i] == 0) {
+                
+                flowerbed[i] = 1; // planta a flor
+                n--;   // decrementa somente se plantar
+        }
+    }
+    // retorna true se todas as flores foram plantadas, false caso contrário
+    return n == 0;
 }
